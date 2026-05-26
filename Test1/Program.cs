73042982451f7664b705089fd4fd6714454a2c88
NodeVortex <166471506+IVELINYTBG1@users.noreply.github.com>
2026-05-26 13:08:19 +0300

    Add files via upload

---
+++ Test1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Test1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test[] Something = { };
+            Stack<int> Stack = new Stack<int>();
+            
+               
+        }
+    }
+
+    
+
+
+}
