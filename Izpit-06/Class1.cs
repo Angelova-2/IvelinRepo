1ef7d128233101826ee5d5cdd80537f4528c8cc4
NodeVortex <166471506+IVELINYTBG1@users.noreply.github.com>
2026-06-16 12:56:29 +0300

    test2

---
+++ Izpit-06/Class1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Xml.Linq;
+
+class Node
+{
+
+    public int Value { get; set; }
+    public Node(int value)
+    {
+        Value = value;
+    }
+    class DynamicNodeArray
+    {
+
+    }
+    private Node[] nodes;
+    private int count;
+    public DynamicNodeArray()
+
+    {
+        int NodesCount = 2;
+        nodes = new Node[NodesCount];
+        count = 0;
+
+    }
+    public void Sort(DynamicNodeArray array)
+    {
+
+    }
+    QuickSortMethod(array, left, right)
+    {
+        int i = 0;
+        int j = 0;
+    }
+
+    public void Add(int value)
+    {
+        string UserInput = Console.ReadLine();
+        foreach (UserInput in list)
+            count++;
+
+    }
+    private void Resize()
+    {
+        if (count > nodes.Length)
+        {
+
+        }
+
+    public void Print()
+    {
+        for (int i = 0; i < nodes.Length; i++)
+        {
+            Console.WriteLine(nodes[i].ToString());
+        }
+    }
+    public int swap(int value)
+    {
+        int i = 0, j = 0;
+        int swap = 0;
+        
+    }
+
+
+
+
+
+//public int Count() { … }
+//public int GetValue(int index) { … }
+//public void SetValue(int index, int value) { … }
---
+++ Izpit-06/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Izpit_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(1);
+            Console.Write(2);
+            Console.Write(5);
+            Console.Write(7);
+            Console.Write(9);
+        }
+    }
+}
